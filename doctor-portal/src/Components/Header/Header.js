0a488e06b4cff4d9423d9ef31b57089235ff5070
shakilmahammud <hshakil987@gmail.com>
2020-10-12 21:15:38 +0600

    home page complete

--- doctor-portal/src/Components/Header/Header.js
+++ doctor-portal/src/Components/Header/Header.js
@@ -5,6 +5,11 @@ import { LearnMore, LearnMoreAppointment } from '../LearnMore/LearnMore';
 import { Navbar } from '../Navbar/Navbar';
 import { Services } from '../Services/Services';
 import './Header.css';
+import Testimonials from '../Testimonials/Testimonials'
+import Blogs from '../Blogs/Blogs';
+import Doctors from '../Doctors/Doctors';
+import Contact from '../Contact/Contact';
+import Footer from '../Footer/Footer';
 
 
 export const Header = () => {
@@ -16,6 +21,11 @@ export const Header = () => {
             <Services/>
             <LearnMore/>
             <LearnMoreAppointment/>
+            <Testimonials/>
+            <Blogs/>
+            <Doctors/>
+            <Contact/>
+            <Footer/>
         </div>
     )
 }
--- doctor-portal/src/Components/Home/Home.js
+++ doctor-portal/src/Components/Home/Home.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import { Header } from '../Header/Header'
+import { Header } from '../Header/Header';
+
 
 export const Home = () => {
     return (
         <div>
-            <Header/>
+            <Header/> 
         </div>
     )
 }
